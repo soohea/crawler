b971c3100777878a9fdcf348ba973405cf1d5cce
soohea <sooheaa@gmai.com>
2022-03-02 15:00:27 +0800

    设计并实现基本的算法 在这个提交中，实现了基本的爬虫算法 虽然还很原始，但我想小步提交

--- src/main/java/com/github/soohea/Main.java
+++ src/main/java/com/github/soohea/Main.java
@@ -6,20 +6,97 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //待处理的链接池
+        List<String> linkPool = new ArrayList<>();
+        //已处理的连接池
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn");
-        try (CloseableHttpResponse response1 = httpClient.execute(httpGet)) {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity = response1.getEntity();
-            System.out.println(EntityUtils.toString(entity));
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            //ArrayList从尾部删除更有效率
+            String link = linkPool.remove(linkPool.size() - 1);
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+
+            if (isInterestingPage(link)) {
+                //处理感兴趣的链接
+
+                Document doc = httpGetAndParseHtml(link);
+
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+
+
+                storeIntoDatabaseIfIsNewsPage(doc);
+
+                processedLinks.add(link);
+            } else {
+
+            }
         }
 
+    }
+
+    private static void storeIntoDatabaseIfIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+            System.out.println(link);
+        }
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
+
+        try (CloseableHttpResponse response1 = httpClient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+
+            String html = EntityUtils.toString(entity1);
+
+            return Jsoup.parse(html);
+
+        }
+    }
+
+    private static boolean isInterestingPage(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLogInPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLogInPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
 
